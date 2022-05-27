b0a2317caf3aba3fe487ec434bb77836793c41bd
AntonPlish <antonplish@gmail.com>
2022-05-27 13:31:33 +0300

    second commit

--- js/task-09.js
+++ js/task-09.js
@@ -1,7 +1,3 @@
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
-
 const btn = document.querySelector('.change-color');
 const body = document.querySelector('body');
 const text = document.querySelector('.color');
